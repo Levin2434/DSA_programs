9332aae2bc040611bd666ac919d9cebc7af01569
Levin2434 <141764492+Levin2434@users.noreply.github.com>
2023-09-11 22:30:06 +0530

    Update 38_P10_Levin.c

--- 38_P10_Levin.c
+++ 38_P10_Levin.c
@@ -1,37 +1,34 @@
-#include <stdio.h> 
- int main() 
- { 
-     int a[10000],b[10000],i,j,n,c=0 ; 
-  
-     printf("Enter size of the array : "); 
-     scanf("%d", &n); 
-  
-     printf("Enter elements in array : "); 
-     for(i=0; i<n; i++) 
-     { 
-         scanf("%d",&a[i]); 
-     } 
-  
-   for(i=0; i<n; i++) { 
-         c=1; 
-           if(a[i]!=-1) { 
-                     for(j=i+1; j<n; j++) { 
-                    if(a[i]==a[j]) 
-                     { 
-                                c++; 
-                                a[j]=-1; 
-                        } 
-                } 
-                b[i]=c; 
-                 } 
-     } 
-     printf("unique numbers in the  array :\n"); 
-         for(i=0; i<n; i++) { 
-          if(a[i]!=-1) 
-          { 
-                 if(b[i]==1) 
-                 printf("%d\n",a[i]); 
-          }  
-     }     
-     return 0; 
- } 
+#include <stdio.h>
+
+int main() {
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int array[size];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    printf("Unique elements in the array: ");
+
+    for (int i = 0; i < size; i++) {
+        int d = 1;
+
+        for (int j = 0; j < size; j++) {
+            if (i != j && array[i] == array[j]) {
+                d = 0;
+                break;
+            }
+        }
+
+        if (d) {
+            printf("%d ", array[i]);
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
