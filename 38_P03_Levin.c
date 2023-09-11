8a6a6969de94cd75e2eee5643944cf3e18ae4128
Levin2434 <141764492+Levin2434@users.noreply.github.com>
2023-09-11 22:38:33 +0530

    Update 38_P03_Levin.c

--- 38_P03_Levin.c
+++ 38_P03_Levin.c
@@ -1,41 +1,36 @@
-#include<stdio.h> 
-  
- /* Declaration of structure */ 
- struct student 
- { 
-     char name[30]; 
-     int roll; 
-     float marks; 
- }; 
-  
- int main() 
- { 
- /* Declaration of array of structure */ 
-     struct student s[20], temp; 
-     int i,j,n; 
-  
-     printf("Enter n:\n"); 
-     scanf("%d",&n); 
-  
- for(i=0;i< n;i++) { 
-     printf("Enter name, roll and marks of student:\n"); 
-     scanf("%s%d%f",s[i].name, &s[i].roll, &s[i].marks); 
- } 
- for(i=0;i< n-1;i++) { 
-     for(j=i+1;j< n;j++) { 
-         if(s[i].marks>s[j].marks) 
-    { 
-     temp = s[i]; 
-     s[i] = s[j]; 
-     s[j] = temp; 
-    } 
-   } 
- } 
-     printf("Sorted records are:\n"); 
-     for(i=0;i< n;i++) { 
-         printf("Name: %s\n", s[i].name); 
-         printf("Roll: %d\n", s[i].roll); 
-         printf("Marks: %0.2f\n\n", s[i].marks); 
-  } 
-  return 0; 
- }
+#include <stdio.h>
+#include <string.h>
+
+struct Student {
+    int roll_no;
+    char name[50];
+    float percentage;
+};
+
+int compare(const void *a, const void *b) {
+    return ((struct Student *)a)->roll_no - ((struct Student *)b)->roll_no;
+}
+
+int main() {
+    struct Student students[5];
+
+    for (int i = 0; i < 5; i++) {
+        printf("Enter details for student %d:\n", i + 1);
+        printf("Roll Number: ");
+        scanf("%d", &students[i].roll_no);
+        printf("Name: ");
+        scanf("%s", students[i].name);
+        printf("Percentage: ");
+        scanf("%f", &students[i].percentage);
+    }
+
+    qsort(students, 5, sizeof(struct Student), compare);
+
+    printf("\nStudent Records (Sorted by Roll Number):\n");
+    printf("Roll No\tName\tPercentage\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d\t%s\t%.2f\n", students[i].roll_no, students[i].name, students[i].percentage);
+    }
+    
+    return 0;
+}
